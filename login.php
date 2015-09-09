f753170f577e7e01ea0edf4f5c72c23f1435ac12
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2015-09-09 13:08:28 +0300

    email error

--- login.php
+++ login.php
@@ -1,5 +1,22 @@
 <?php
 	
+	//echo $_POST["email"];
+	$email_error = "";
+	
+	//kontrollime, et keegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+		//echo "keegi vajutas nuppu";
+		
+		//kontrollin, et e-post ei ole tühi
+		
+		if ( empty($_POST["email"])){
+			$email_error = "See väli on kohustuslik";
+			
+			
+		}
+		
+	}
 
 ?>
 <html>
@@ -9,9 +26,11 @@
 <body>
 	<h2>Log in</h2>
 	
-		<input type="email" placeholder="E-post">  <br><br>
-		<input type="password" placeholder="parool">  <br><br>
-		<input type="submit" value="Login">  <br><br>
+		<form action="login.php" method="post" >
+			<input name="email" type="email" placeholder="E-post">  <?php echo $email_error; ?><br><br>
+			<input name="password" type="password" placeholder="parool">  <br><br>
+			<input type="submit" value="Login">  <br><br>
+		</form>
 	
 	<h2>Create user</h2>
 	
