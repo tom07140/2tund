3e3f588b698ea8a7c77b09dd7f7f1bd2d7d93a57
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2015-09-09 11:33:04 +0300

    added logic

--- helloworld.php
+++ helloworld.php
@@ -4,3 +4,24 @@
 
 	echo $first_name." ".$last_name;
 ?>
+<br>
+<?php
+
+	$age = 20;
+	
+	//testib loogikat, juhul kui vanus on väiksem kui 18 siis kirjuta "alaealine"
+	//muul juhul "täisealine"
+	
+	if($age < 18) {
+		//tõene
+		
+		echo "alaealine";
+		
+	} else {
+		//väär
+		
+		echo "täisealine";
+		
+	}
+
+?>
