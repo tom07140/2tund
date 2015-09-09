8db1f540addc6e5c8d1f59e236d75041af728950
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2015-09-09 12:38:19 +0300

    added login page and form

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+	
+
+?>
+<html>
+<head>
+	<title>Login page</title>
+</head>
+<body>
+	<h2>Log in</h2>
+	
+		<input type="email" placeholder="E-post">  <br><br>
+		<input type="password" placeholder="parool">  <br><br>
+		<input type="submit" value="Login">  <br><br>
+	
+	<h2>Create user</h2>
+	
+</body>
+
+</html>
